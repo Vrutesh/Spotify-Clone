e4bb064f9e52f1ed88efff7ca665c689e1ab63ac
Vrutesh Mayekar <vruteshpm02@gmail.com>
2024-02-14 11:05:56 +0530

    fix : added song duration and play, pause

--- Code/script.js
+++ Code/script.js
@@ -1,35 +1,48 @@
-console.log("Let's write Javascript Here")
+console.log("Let's write Javascript Here");
+let currentSong = new Audio();
 
+async function getSongs() {
+  let a = await fetch("/Code/songs/");
+  let response = await a.text();
+  // console.log(response)
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  // console.log(as)
+  let songs = [];
 
-async function getSongs(){
-    let a = await fetch("http://127.0.0.1:5501/Code/songs/")
-    let response = await a.text()
-    console.log(response)
-    let div = document.createElement("div")
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
-    // console.log(as)
-    let songs =[]
-    
-    for (let i = 0; i < as.length; i++) {
-        const element = as[i];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href.split("/songs/")[1])
-        }
-        
+  for (let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
     }
-    return songs
+  }
+  return songs;
 }
 
+const playMusic = (track, pause=false) => {
+  currentSong.src = "/Code/songs/" + track;
+  if(!pause){
+    currentSong.play();
+    play.src = "assets/pause.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = decodeURI(track)
+  document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+};
 
-async function main(){
-    let songs = await getSongs()
-    console.log(songs)
 
-    let songUL= document.querySelector(".song-library").getElementsByTagName("ul")[0]
-    // console.log(songUL)
-    for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li>
+async function main() {
+  let songs = await getSongs();
+  playMusic(songs[0], true)
+
+  let songUL = document
+    .querySelector(".song-library")
+    .getElementsByTagName("ul")[0];
+  // console.log(songUL)
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li>
         <img class="invert" src="/Code/assets/music.svg" alt="">
         <div class="info">
                     <div>${song}</div>
@@ -38,20 +51,60 @@ async function main(){
                   <div class="playnow">
                     <span>Play Now</span>
                     <img class="invert" src="/Code/assets/play.svg" alt="">
-                  </div>
-                
-        
-        
-        </li>`
-    }
+                  </div> </li>`;
+  }
+  Array.from(
+    document.querySelector(".song-library").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
 
-    var audio = new Audio(songs[0]);
-    audio.play();
 
-    audio.addEventListener("loadeddata", ()=>{
-        let duration= audio.duration
-        console.log(audio.duration, audio.currentSrc, audio.currentTime)
-    })
+  //  event listener to play, pause, next & previous
+
+  const play = document.getElementById("play");
+  play.addEventListener("click", () => {
+      if (currentSong.paused) {
+          currentSong.play();
+          play.src = "assets/pause.svg";
+      } else {
+          currentSong.pause();
+          play.src = "assets/play.svg";
+      }
+  });
+  
+
+
+  // listen event on song duration 
+  
+  currentSong.addEventListener("timeupdate", () => {
+    console.log(currentSong.currentTime, currentSong.duration);
+    const currentTimeFormatted = secondsToMinutesSeconds(currentSong.currentTime);
+    const durationFormatted = secondsToMinutesSeconds(currentSong.duration);
+    document.querySelector(".songtime").innerHTML = `${currentTimeFormatted}/${durationFormatted}`;
+});
+
+function secondsToMinutesSeconds(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
 
-main()
+}
+
+main();
+
+
+
+// login and signup 
+
+let login = document.getElementById("login").addEventListener("click", () => {
+  window.location.href = "login.html";
+});
+
+let signup = document.getElementById("signup").addEventListener("click", () => {
+  window.location.href = "signup.html";
+});
